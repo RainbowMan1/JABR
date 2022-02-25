6a6f8e660aaf53e6bae7dd7cb5a0bf936b6181c8
RainbowMan1 <nikeshsubedi2@gmail.com>
2022-02-25 16:41:15 -0500

    barebones menu

---
+++ MainMenu/mainMenu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"Button Container/Play Button".grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Play_Button_pressed():
+	# TODO: Replace following with the first scene
+	# Game.goto_scene_fade();
+	pass
+
+func _on_Quit_Button_pressed():
+	get_tree().quit()
