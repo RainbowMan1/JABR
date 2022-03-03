30e5b91b824e072adc987ad89a96544dac4c78b1
Procrascat <khalil.luke@gmail.com>
2022-03-03 12:33:22 -0500

    Timer and Scores
    Added Timer to Arena and Score collection and calculation. Currently scores don't display in the build.

--- Arena.gd
+++ Arena.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-var fsm: WeakRef
-
+onready var fsm = $StateMachine
+var clock = 0 
+var time_multi = 1.0 
+var paused = false
+onready var clockText = get_node("ClockText")
+onready var playerHealth = get_node("PlayerBase").health
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,13 +13,18 @@ var fsm: WeakRef
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(true)
 	pass # Replace with function body.
 
 
 func updateGameState():
 	fsm.get_ref().updateGameState()
-	
+	#fsm.scoreData(clock, playerHealth)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#Updates clock text every second
+	clock += delta * time_multi 
+	clockText.text = "Time: " + str(int(clock)) + "sec"
+	
+	pass
